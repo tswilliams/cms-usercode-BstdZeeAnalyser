3f1b40926cdd85037c6356de6be3f43b8b1a236a
Thomas <t.williams@cern.ch>
2014-05-08 16:31:18 +0100

    Update signal MC distributions script for thesis

--- macros/makeSignalMcPlots.C
+++ macros/makeSignalMcPlots.C
@@ -1,26 +1,43 @@
 {
 
+  std::string outPrefix = "results/20140508";
+
    tsw::Samples2012 samples("zEffiTree");
    std::string selnString = "(abs(mcZ_ele1_p4.Eta())<1.44 && abs(mcZ_ele2_p4.Eta())<1.44) && (mcZ_ele1_p4.Et()>35.0 && mcZ_ele2_p4.Et()>35.0)";
    tsw::DistPlotter plotter(true);
+
    plotter.setTree("zBosonEffiTree");
    plotter.setSelection(selnString);
-   plotter.descriptiveText("MC truth after acceptance cuts; gauge interaction.");
-   plotter.outFilePrefix("results/20130106/qStarSignalMcAfterAcc_");
+   //plotter.descriptiveText("MC truth after acceptance cuts; gauge interaction.");
+   plotter.outFilePrefix( outPrefix + "/qStarGI_afterAcc_" );
    plotter.rescaleMC();
 
+   plotter.add( tsw::AxisDefn("ZpT", 90, 0.0, 1800, "Z boson p_{T} [GeV]") );
+   plotter.add( tsw::AxisDefn("ZdR", 120, 0.0, 1.2, "#DeltaR_{ee}") );
+
+   tsw::DistPlotter plotterCI(plotter);
+   plotterCI.outFilePrefix( outPrefix + "/qStarCI_afterAcc_" );
+
+
    tsw::MCSample qStarM1500( samples.qStarGI_M1500() );
-   qStarM1500.mName  = "1.5TeV q*";
-   //qStarM1500.mColor = tsw::Black;
+   qStarM1500.mName  = "1.5TeV q* (GI)";
+   qStarM1500.mColor = tsw::Black;
    tsw::MCSample qStarM2500( samples.qStarGI_M2500() );
-   qStarM2500.mName = "2.5TeV q*";
+   qStarM2500.mName = "2.5TeV q* (GI)";
+
+   tsw::MCSample qStarCI_M1500( samples.qStarCI_M1500() );
+   qStarCI_M1500.mName  = "1.5TeV q* (CI)";
+   tsw::MCSample qStarCI_M2500( samples.qStarCI_M2500() );
+   qStarCI_M2500.mName = "2.5TeV q* (CI)";
+   
+
 
    plotter.add( qStarM1500 );
-   //plotter.add( samples.qStarGI_M2000() );
    plotter.add( qStarM2500 );
-   plotter.add( tsw::AxisDefn("ZpT", 40, 400.0, 1400, "Z boson p_{T} [GeV]") );
-   plotter.add( tsw::AxisDefn("ZdR", 96, 0.0, 1.2, "#DeltaR_{ee}") );
-   //plotter.add( tsw::AxisDefn("Zp4.M()", 120, 60.0, 120.0, "M_{ee} [GeV]") );
    plotter.run();
+
+   plotterCI.add( qStarCI_M1500 );
+   plotterCI.add( qStarCI_M2500 );
+   plotterCI.run();
 
 }
