a9d536c8a0cb18b5cfad01d489ed193e4e02a5ee
Thomas <t.williams@cern.ch>
2014-05-31 19:11:46 +0100

    Add files for calculating signal cross sections

---
+++ qstar_cross_sections.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mode='CI'
+masses=`seq 2000 100 3000`
+
+for mass in $masses; do
+  echo "Creating generator file: python/Qstar${mode}ToQZ_M_${mass}_8TeV_pythia6_cff.py"
+  sed "s/.*PMAS.343.*/            'PMAS\(343,1\) = $mass  \!mass of d*',/" python/Qstar${mode}ToQZ_M_XXXX_8TeV_pythia6_cff.py \
+    | sed "s/.*PMAS.344.*/            'PMAS\(344,1\) = $mass  \!mass of u*',/" \
+    | sed "s/.*RTCM.41.*/            'RTCM\(41\)    = $mass  \!Lambda = mass ',/" \
+    > python/Qstar${mode}ToQZ_M_${mass}_8TeV_pythia6_cff.py 
+done
+
+echo; echo
+scram b 
+
+for mass in $masses; do
+  echo; echo; echo "-> Calculating cross section at mass $mass"
+  cmsDriver.py TSWilliams/BstdZeeAnalyser/python/Qstar${mode}ToQZ_M_${mass}_8TeV_pythia6_cff.py -s GEN --conditions auto:mc --datatier 'GEN-SIM-RAW' --eventcontent RAWSIM --fileout /opt/ppd/newscratch/williams/tmp.root -n 1000 \
+    2>&1 | grep "All included subprocesses"
+done
+
